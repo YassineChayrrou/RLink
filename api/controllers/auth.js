cc84328c538c21b62b4326118c4ce5656503ba4f
YassineChayrrou <yassine.chayrrou@gmail.com>
2021-09-12 16:47:04 +0100

    added jsonwebtoken for authorization features once verified or logged in

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -3,7 +3,6 @@ const ErrorResponse = require("../utils/errorResponse");
 
 /* Register block */
 exports.register = async (req, res, next) => {
-  // res.send("Register Route");
   const { username, email, password } = req.body;
   // console.log(req.body);
 
@@ -13,10 +12,8 @@ exports.register = async (req, res, next) => {
       email,
       password,
     });
-    res.status(201).json({
-      success: true,
-      user: user,
-    });
+
+    await sendToken(user, 201, res);
   } catch (error) {
     res.status(500).json({
       success: false,
@@ -27,19 +24,14 @@ exports.register = async (req, res, next) => {
 
 /* Login block */
 exports.login = async (req, res, next) => {
-  // res.send("Login Route");
   const { email, password } = req.body;
 
-  if (!email && !password) {
+  if (!email || !password) {
     // res.status(400).json({
     //   success: false,
     //   error: "Please provide and email or password",
     // });
-    return next(new ErrorResponse("Missing email and password", 401));
-  }
-
-  if (!password) {
-    return next(new ErrorResponse("Missing password"));
+    return next(new ErrorResponse("Missing email or password", 401));
   }
 
   try {
@@ -59,10 +51,9 @@ exports.login = async (req, res, next) => {
       // });
       return next(new ErrorResponse("Invalid Credentials", 404));
     }
-    res.status(200).json({
-      success: true,
-      token: "tr3444433",
-    });
+
+    //generates a jsonwebtoken and send it as response
+    await sendToken(user, 201, res);
   } catch (error) {
     res.status(500).json({
       success: false,
@@ -79,4 +70,10 @@ exports.forgotPassword = (req, res, next) => {
 /* ResetPassword Block*/
 exports.resetPassword = (req, res, next) => {
   res.send("Reset Password Route");
+};
+
+const sendToken = (user, statusCode, res) => {
+  const token = user.getToken();
+  console.log(token);
+  res.status(statusCode).json({ success: true, token });
 };
