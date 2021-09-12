042e4bb3ae01d0e5333beab322981e39429dc915
YassineChayrrou <yassine.chayrrou@gmail.com>
2021-09-13 00:49:50 +0100

    setup authorization and path protection for private routes

--- api/models/User.js
+++ api/models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const crypto = require("crypto");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
@@ -38,10 +39,20 @@ UserSchema.methods.isValidPassword = async function (password) {
   return await bcrypt.compare(password, this.password);
 };
 
-UserSchema.methods.getToken = async function () {
+UserSchema.methods.getToken = function () {
   return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
     expiresIn: process.env.JWT_EXPIRE,
   });
+};
+
+UserSchema.methods.getResetPasswordToken = function () {
+  const resetToken = crypto.randomBytes(35).toString("hex");
+  this.resetPasswordToken = crypto
+    .createHash("sha256")
+    .update(resetToken)
+    .digest("hex");
+
+  this.resetPasswordExpire = Date.now() + 2 * 60 * (60 * 1000);
 };
 
 const User = mongoose.model("User", UserSchema);
