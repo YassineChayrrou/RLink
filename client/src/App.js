1b2aa6743379d05c563cae6522779d98acf17bfb
YassineChayrrou <yassine.chayrrou@gmail.com>
2021-09-15 09:47:27 +0100

    updated all public routes and private routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,38 @@
 import React, { StrictMode } from "react";
 import { render } from "react-dom";
 import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
-// import "./css/slate-bootstrap.min.css";
-// import "./css/superhero-bootstrap.min.css";
-// import "./css/lux-bootstrap.min.css";
-// import "./css/slate-bootstrap.min.css";
 import "./css/bootstrap.min.css";
+//Public screens
 import Header from "./components/Header";
 import Home from "./components/Home";
-import Control from "./components/ros/Control";
 import About from "./components/About";
+//Private screens
+import PrivatePath from "./components/authentication/routing/PrivatePath";
+import Control from "./components/ros/Control";
+import Login from "./components/authentication/screens/Login";
+import Register from "./components/authentication/screens/Register";
+import ForgotPassword from "./components/authentication/screens/ForgotPassword";
+import ResetPassword from "./components/authentication/screens/ResetPassword";
 
 const App = () => {
   return (
     <div>
       <Router>
         <Switch>
+          <PrivatePath exact path="/control" component={Control} />
           <Route path="/" exact component={Home}></Route>
-          <Route path="/control" component={Control}></Route>
+          <Route path="/login" exact component={Login}></Route>
+          <Route path="/register" exact component={Register}></Route>
+          <Route
+            path="/forgotpassword"
+            exact
+            component={ForgotPassword}
+          ></Route>
+          <Route
+            path="/resetpassword/:resetToken"
+            exact
+            component={ResetPassword}
+          ></Route>
           <Route path="/about" component={About}></Route>
         </Switch>
       </Router>
